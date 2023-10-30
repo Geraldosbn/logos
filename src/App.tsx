6b981404a4420560d60d06bdf33d30f6a9d64245
geraldosbn <geraldosbn92@gmail.com>
2023-10-30 18:07:29 -0300

    fix: teste rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { ChurchHistory } from './pages/ChurchHistory/ChurchHistory.tsx'
 import Home from './pages/Home/Home.tsx'
 import { ChildrensArea } from './pages/ChildrensArea/ChildrensArea.tsx'
@@ -7,14 +7,16 @@ import { Main } from './pages/Main/Main.tsx'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Main />}>
-        <Route index path='/' element={<Home />} />
-        <Route path='/churchHistory' element={<ChurchHistory />} />
-        <Route path='/ruleOfFaith' element={<RulesOfFaith />} />
-        <Route path='/childrensArea' element={<ChildrensArea />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />}>
+          <Route index path='/' element={<Home />} />
+          <Route path='/churchHistory' element={<ChurchHistory />} />
+          <Route path='/ruleOfFaith' element={<RulesOfFaith />} />
+          <Route path='/childrensArea' element={<ChildrensArea />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
 import App from './App.tsx'
@@ -12,11 +12,9 @@ import globalTheme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <BrowserRouter>
-      <ThemeProvider theme={globalTheme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={globalTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
