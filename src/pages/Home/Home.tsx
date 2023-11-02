7e83b3e5ffebe85dd9889b422f1f28610a7d7f0f
geraldosbn <geraldosbn92@gmail.com>
2023-11-02 15:13:43 -0300

    build: add circular progress in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from '@mui/material'
+import { CircularProgress, Typography } from '@mui/material'
 import { Card } from '../../components/Card/Card'
 import { useHome } from './service/useHome'
 import { useStyles } from './style'
@@ -7,15 +7,18 @@ function Home() {
   const classes = useStyles()
   const { data: postsFromApi, isFetching } = useHome()
 
+  console.log('postsFromApi', postsFromApi)
+
   if (isFetching) return <CircularProgress />
   if (postsFromApi && postsFromApi?.length <= 0)
-    return (
-      <div className={classes.container}>
-        {postsFromApi?.map(({ author, content, description, title }, index) => (
-          <Card key={index} post={{ author, content, description, title }} />
-        ))}
-      </div>
-    )
+    return <Typography>Nenhum post por enquanto.</Typography>
+  return (
+    <div className={classes.container}>
+      {postsFromApi?.map(({ author, content, description, title }, index) => (
+        <Card key={index} post={{ author, content, description, title }} />
+      ))}
+    </div>
+  )
 }
 
 export default Home
