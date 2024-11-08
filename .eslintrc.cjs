37a745dece6e1c1c168f40709e0b2a91746d18c7
geraldosbn <geraldosbn92@gmail.com>
2024-11-08 10:11:07 -0300

    config: atualizando config prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-	root: true,
-	env: {browser: true, es2020: true},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
-	rules: {
-		'react-hooks/exhaustive-deps': 'off'
-	}
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-empty': 'off'
+  }
 }
