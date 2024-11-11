835dde8c59a83131949e355cbe0c4fecd947a484
geraldosbn <geraldosbn92@gmail.com>
2024-11-11 14:54:48 -0300

    feat: retirna parte do form que cadastrava usuario

--- src/pages/MemberRegister/MemberRegister.tsx
+++ src/pages/MemberRegister/MemberRegister.tsx
@@ -1,40 +1,44 @@
-import {useEffect, useState} from 'react'
-import {Typography} from '@mui/material'
-import {schemaForm} from './schemaForm'
-import {useStyles} from './style'
-import {useMemberRegister} from './service/useMemberRegister'
-import {useFormValidate} from '../../hooks/useFormValidate'
-import {InputTextField} from '../../components/InputTextField/InputTextField'
-import {Button} from '../../components/Button/Button'
-import {CheckBox} from '../../components/CheckBox/CheckBox'
-import {PaperForm} from '../../components/PaperForm/PaperForm'
-import {InputDate} from '../../components/InputDate/InputDate'
-import {MemberData} from '../../shared/interfaces/interfaces'
-import {InputMask} from '../../components/InputMask/InputMask'
+import { useEffect } from 'react'
+import { Typography } from '@mui/material'
+import { schemaForm } from './schemaForm'
+import { useStyles } from './style'
+import { useMemberRegister } from './service/useMemberRegister'
+import { useFormValidate } from '../../hooks/useFormValidate'
+import { InputTextField } from '../../components/InputTextField/InputTextField'
+import { Button } from '../../components/Button/Button'
+import { PaperForm } from '../../components/PaperForm/PaperForm'
+import { InputDate } from '../../components/InputDate/InputDate'
+import { InputMask } from '../../components/InputMask/InputMask'
+import { MemberData, MemberDataInput } from '../../shared/interfaces/interfaces'
 
 export const MemberRegister = () => {
   const classes = useStyles()
-  const {mutateAsync: onSubmit, isSuccess, reset} = useMemberRegister()
+  const { mutateAsync: onSubmit, isSuccess, reset } = useMemberRegister()
 
-  const [isUserSystem, setIsUserSystem] = useState(false)
-  const [passwords, setPasswords] = useState<{
-    password: string
-    confirmPassword: string
-  }>({password: '', confirmPassword: ''})
-
-  const {register, errors, handleSubmit, setValue, clearErrors} = useFormValidate(
-    schemaForm({
-      isUserSystem,
-      passwordError: passwords.password !== passwords.confirmPassword
-    })
-  )
+  const { register, errors, handleSubmit, setValue, clearErrors } = useFormValidate(schemaForm())
 
   useEffect(() => {
     isSuccess && reset()
   }, [isSuccess])
 
-  const handleSubmitForm = (data: MemberData) => {
-    onSubmit(data)
+  const handleSubmitForm = (data: MemberDataInput) => {
+    const { name, phone, birthday, cpf, rg, emissorRG } = data
+    const memberData: MemberData = {
+      person: {
+        name,
+        birthday,
+        personData: {
+          phone,
+          personDocuments: {
+            cpf,
+            rg,
+            emissorRG
+          }
+        }
+      }
+    }
+
+    onSubmit(memberData)
   }
 
   return (
@@ -42,7 +46,7 @@ export const MemberRegister = () => {
       <Typography className={classes.title}>Cadastrar membro</Typography>
       <InputTextField
         label="Nome"
-        inputProps={{...register('name')}}
+        inputProps={{ ...register('name') }}
         error={!!errors.name}
         helperText={errors.name?.message as string}
       />
@@ -64,9 +68,25 @@ export const MemberRegister = () => {
         setValueOnBlur={(value) => {
           setValue('cpf', value)
         }}
-        inputProps={{...register('cpf')}}
+        inputProps={{ ...register('cpf') }}
         error={!!errors.cpf}
         helperText={errors.cpf?.message as string}
+      />
+      <InputMask
+        label="RG"
+        type="rg"
+        setValueOnBlur={(value) => {
+          setValue('rg', value)
+        }}
+        inputProps={{ ...register('rg') }}
+        error={!!errors.rg}
+        helperText={errors.rg?.message as string}
+      />
+      <InputTextField
+        label="Emissor RG"
+        inputProps={{ ...register('emissorRG') }}
+        error={!!errors.emissorRG}
+        helperText={errors.emissorRG?.message as string}
       />
       <InputDate
         label="Data de nascimento"
@@ -77,44 +97,6 @@ export const MemberRegister = () => {
         clearError={() => clearErrors('birthday')}
         error={!!errors.birthday}
         helperText={errors.birthday?.message as string}
-      />
-      {isUserSystem && (
-        <div style={{width: '100%'}}>
-          <InputTextField
-            label="Login"
-            autoComplete="username"
-            inputProps={{...register('username')}}
-            error={!!errors.username}
-            helperText={errors.username?.message as string}
-          />
-          <InputTextField
-            label="Senha"
-            type="password"
-            autoComplete="new-password"
-            onChange={(evt) => setPasswords({...passwords, password: evt.target.value})}
-            inputProps={{...register('password')}}
-            error={!!errors.password}
-            helperText={errors.password?.message as string}
-          />
-          <InputTextField
-            label="confirme a senha"
-            type="password"
-            autoComplete="new-password"
-            onChange={(evt) =>
-              setPasswords({
-                ...passwords,
-                confirmPassword: evt.target.value
-              })
-            }
-            inputProps={{...register('confirmPassword')}}
-            error={!!errors.confirmPassword}
-            helperText={errors.confirmPassword?.message as string}
-          />
-        </div>
-      )}
-      <CheckBox
-        label="Este membro é um usuario do sistema?"
-        onclick={setIsUserSystem}
       />
       <Button type="submit">Salvar</Button>
     </PaperForm>
